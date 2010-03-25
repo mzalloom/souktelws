a351aa883fdaf0e092f9ac03b6096f226055cd0b
Mohannad Zalloom <mzalloom@mohannad.(none)>
2010-03-25 12:23:54 +0200

    Add locale to survey

--- apps/survey/app.py
+++ apps/survey/app.py
@@ -5,8 +5,8 @@
 import rapidsms
 from rapidsms.parsers.keyworder import Keyworder
 from models import Profile, Activities
-
-
+from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 class App(rapidsms.app.App):
 
     keyword = Keyworder()
@@ -24,41 +24,42 @@ class App(rapidsms.app.App):
             message.respond(u"System encountered an Error: %s" % e)
             return True
 
-    keyword.prefix = ['Help']
+    keyword.prefix = ['Help', _(u"help")]
     @keyword(r'')
     def help(self, message):
-        message.respond(u"Please enter the follow info [Surname,Name,Sex,Age(M:Male,F:Female),Acitivity(ST:Student,WR:Worker)]")
+        message.respond(_(u"Please enter the follow info [Surname,Name,Sex,Age(M:Male,F:Female),Acitivity(ST:Student,WR:Worker)]"))
         return True
     
-    keyword.prefix = ['survey'] 
+    keyword.prefix = ['survey', _(u"survey")]
     @keyword(r'([a-z]+) ([a-z]+) ([M|F]) ([0-9]+) ([a-z]+)')
     def sery(self, message, first, last, sex, age, Act):
-
+        message.respond(sex)
         Act1 = Activities.objects.get(code =Act)
         prof = Profile(First_Name = first, Last_Name = last, Sex = sex, Age = age, Activity = Act1)
         
         try : 
             prof.save()
-            message.respond(u"success")
+            message.respond(_(u"success"))
         except:
-            message.repond(u"Error")
+            message.repond(_(u"Error"))
             
         return True
 
-    keyword.prefix = ['find']
+    keyword.prefix = ['find', _(u"find")]
     @keyword(r'([a-z]+)')
     def find(self, message , keyw):
         Act1 = Activities.objects.get(code =keyw)
         prof1 = Profile.objects.get(Activity=Act1)
 
-        message.respond(u"Hello %s" % prof1.First_Name)
+        message.respond(_(u"Hello %s") % prof1.First_Name)
 
         return True
 
-    keyword.prefix = ['stat']
+    keyword.prefix = ['stat', _(u"stat")]
     @keyword(r'')
     def find(self, message):
         
-        message.respond(u"Number of Records %s" % Profile.objects.count())
+        message.respond(_(u"Number of Records %s") % Profile.objects.count())
+
 
         return True
--- apps/survey/models.py
+++ apps/survey/models.py
@@ -4,22 +4,22 @@
 
 
 from django.db import models
-
+from django.utils.translation import ugettext_lazy as _
 
 class Activities(models.Model) :
 
-    code = models.CharField(max_length=5, unique=True)
-    name= models.CharField(max_length=20)
+    code = models.CharField(max_length=5, unique=True, verbose_name=_(u"Code"))
+    name= models.CharField(max_length=20, verbose_name=_(u"Name"))
 
     def __unicode__(self):
 	    return u"%s (%s)"% (self.code,self.name)
 
 class Profile(models.Model) :
 
-    First_Name = models.CharField("This is First",max_length=20, unique=True)
-    Last_Name = models.CharField(max_length=20)
-    Sex	= models.CharField(max_length=1)
-    Age = models.CharField(max_length=5)
+    First_Name = models.CharField(max_length=20, unique=True, verbose_name=_(u"First Name"))
+    Last_Name = models.CharField(max_length=20, verbose_name=_(u"Last Name"))
+    Sex	= models.CharField(max_length=1, verbose_name=_(u"Sex"))
+    Age = models.CharField(max_length=5, verbose_name=_(u"Age"))
     Activity = models.ForeignKey(Activities)
     
     def __unicode__(self):
